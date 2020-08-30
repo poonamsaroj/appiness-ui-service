fa7866e894e7a7e2b8c5c5c77d96bdbba2ce7d3c
poonamsaroj <sarojpoonam30@gmail.com>
2020-08-30 12:58:31 +0530

    First commit

---
+++ src/app/core/api.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface GitHubUserList {
+  id: number,
+  login: String,
+  avatar_url: String
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ApiService {
+
+  private url = 'https://api.github.com/users?accept=application/vnd.github.v3+json&username=sarojpoonam30@gmail.com&password={}';  // URL to web api
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  /** GET GITHUB USERS from the server */
+  getList(): Observable<GitHubUserList[]> {
+    return this.http.get<GitHubUserList[]>(this.url)
+  }
+
+}
