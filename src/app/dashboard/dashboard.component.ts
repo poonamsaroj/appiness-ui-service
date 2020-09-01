5b1bf54495b94aa59e60e5d294b61bead32d7551
poonamsaroj <sarojpoonam30@gmail.com>
2020-09-02 02:15:28 +0530

    Added global search

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,13 +7,26 @@ import { ApiService } from 'app/core/api.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  dataval
+  dataval;
+  datavalClone;
+
   constructor(private data: ApiService) { }
 
+  // Global filter on table
+  applyFilter(filterValue: string) {
+    this.dataval = this.datavalClone
+    if (this.dataval !== undefined) {
+      const newdata = this.dataval.filter(name => name['login'].trim().toLowerCase().includes(filterValue.trim().toLowerCase()))
+      this.dataval = newdata;
+    }
+    if(filterValue == '') this.dataval = this.datavalClone
+  }
+
   ngOnInit() {
-    debugger
+    // Fetching link data
     this.data.getList().subscribe(data => {
       this.dataval = data;
+      this.datavalClone = data;
     });
   }
 
