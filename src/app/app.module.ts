7c53f8410c15fc80d67814e318093c70d4a80b28
poonamsaroj <sarojpoonam30@gmail.com>
2020-09-02 01:16:33 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,8 @@ import { ComponentsModule } from './components/components.module';
 
 import { AppComponent } from './app.component';
 
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { TableListComponent } from './table-list/table-list.component';
-import { TypographyComponent } from './typography/typography.component';
-import { IconsComponent } from './icons/icons.component';
-import { MapsComponent } from './maps/maps.component';
-import { NotificationsComponent } from './notifications/notifications.component';
-import { UpgradeComponent } from './upgrade/upgrade.component';
+import { HttpClientModule } from '@angular/common/http';
+
 import {
   AgmCoreModule
 } from '@agm/core';
@@ -31,6 +25,7 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
     HttpModule,
     ComponentsModule,
     RouterModule,
+    HttpClientModule,
     AppRoutingModule,
     AgmCoreModule.forRoot({
       apiKey: 'YOUR_GOOGLE_MAPS_API_KEY'
---
+++ src/app/core/api.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface GitHubUserList {
+  id: number,
+  login: String,
+  avatar_url: String
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ApiService {
+
+  private url = 'https://api.github.com/users?accept=application/vnd.github.v3+json&username=sarojpoonam30@gmail.com&password=Pika@1298';  // URL to web api
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  /** GET GITHUB USERS from the server */
+  getList(): Observable<GitHubUserList[]> {
+    debugger
+    return this.http.get<GitHubUserList[]>(this.url)
+  }
+
+}
